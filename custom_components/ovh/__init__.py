af682d3acdc044c9ad33b2fccf126967f977cada
GuilleGF <guillermogf@gmail.com>
2020-07-26 13:32:27 +0200

    OVH DynDNS

--- custom_components/ovh/__init__.py
+++ custom_components/ovh/__init__.py
@@ -1,11 +1,10 @@
-"""Integrate with NO-IP Dynamic DNS service."""
+"""Integrate with OVH Dynamic DNS service."""
 import asyncio
-import base64
 from datetime import timedelta
 import logging
 
 import aiohttp
-from aiohttp.hdrs import AUTHORIZATION, USER_AGENT
+from aiohttp.hdrs import USER_AGENT
 import async_timeout
 import voluptuous as vol
 
@@ -15,7 +14,7 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "no_ip"
+DOMAIN = "ovh"
 
 # We should set a dedicated address for the user agent.
 EMAIL = "hello@home-assistant.io"
@@ -24,16 +23,16 @@ INTERVAL = timedelta(minutes=5)
 
 DEFAULT_TIMEOUT = 10
 
-NO_IP_ERRORS = {
+OVH_ERRORS = {
     "nohost": "Hostname supplied does not exist under specified account",
     "badauth": "Invalid username password combination",
     "badagent": "Client disabled",
     "!donator": "An update request was sent with a feature that is not available",
     "abuse": "Username is blocked due to abuse",
-    "911": "A fatal error on NO-IP's side such as a database outage",
 }
 
-UPDATE_URL = "https://dynupdate.noip.com/nic/update"
+UPDATE_URL = "https://www.ovh.com/nic/update"
+IP_URL = "https://api.ipify.org"
 HA_USER_AGENT = f"{SERVER_SOFTWARE} {EMAIL}"
 
 CONFIG_SCHEMA = vol.Schema(
@@ -52,57 +51,57 @@ CONFIG_SCHEMA = vol.Schema(
 
 
 async def async_setup(hass, config):
-    """Initialize the NO-IP component."""
+    """Initialize the OVH component."""
     domain = config[DOMAIN].get(CONF_DOMAIN)
     user = config[DOMAIN].get(CONF_USERNAME)
     password = config[DOMAIN].get(CONF_PASSWORD)
     timeout = config[DOMAIN].get(CONF_TIMEOUT)
 
-    auth_str = base64.b64encode(f"{user}:{password}".encode("utf-8"))
-
     session = hass.helpers.aiohttp_client.async_get_clientsession()
 
-    result = await _update_no_ip(hass, session, domain, auth_str, timeout)
+    result = await _update_ovh(hass, session, domain, user, password, timeout)
 
     if not result:
         return False
 
     async def update_domain_interval(now):
-        """Update the NO-IP entry."""
-        await _update_no_ip(hass, session, domain, auth_str, timeout)
+        """Update the OVH entry."""
+        await _update_ovh(hass, session, domain, user, password, timeout)
 
     hass.helpers.event.async_track_time_interval(update_domain_interval, INTERVAL)
 
     return True
 
 
-async def _update_no_ip(hass, session, domain, auth_str, timeout):
-    """Update NO-IP."""
+async def _update_ovh(hass, session, domain, user, password, timeout):
+    """Update OVH."""
     url = UPDATE_URL
 
-    params = {"hostname": domain}
+    ip = session.get(IP_URL).text
+    _LOGGER.info("Public IP: {}".format(ip))
 
-    headers = {
-        AUTHORIZATION: f"Basic {auth_str.decode('utf-8')}",
-        USER_AGENT: HA_USER_AGENT,
-    }
+    params = {"myip": ip, "system": "dyndns", "hostname": domain}
+
+    headers = {USER_AGENT: HA_USER_AGENT}
+
+    authentication = hass.helpers.aiohttp_client.BasicAuth(user, password)
 
     try:
         with async_timeout.timeout(timeout):
-            resp = await session.get(url, params=params, headers=headers)
+            resp = await session.get(url, params=params, headers=headers, auth=authentication)
             body = await resp.text()
 
             if body.startswith("good") or body.startswith("nochg"):
                 return True
 
             _LOGGER.warning(
-                "Updating NO-IP failed: %s => %s", domain, NO_IP_ERRORS[body.strip()]
+                "Updating OVH failed: %s => %s", domain, OVH_ERRORS[body.strip()]
             )
 
     except aiohttp.ClientError:
-        _LOGGER.warning("Can't connect to NO-IP API")
+        _LOGGER.warning("Can't connect to OVH API")
 
     except asyncio.TimeoutError:
-        _LOGGER.warning("Timeout from NO-IP API for domain: %s", domain)
+        _LOGGER.warning("Timeout from OVH API for domain: %s", domain)
 
     return False
